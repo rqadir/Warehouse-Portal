6110cf130060a45de9e0db67d569ac0348ccbbde
drraq <diracist@gmail.com>
2019-09-20 18:25:50 +0500

    Initial Files. Fri, 20 Sep 2019

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Daraz Warehouse Portal</title>
+  <link rel="stylesheet" href="/css/master.css">
+  <link rel="stylesheet" href="/css/index.css">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <div class="logo">
+      <a href="/"><img src="/imgs/dwh_logo.png" alt="Daraz Warehouse Logo" height="60px"></a>
+    </div>
+  </header>
+  <!-- Buttons to Execute a job and Download a file -->
+
+  <div class="buttons sspro f16 bgColor">
+    <div class="btn">
+      <a href="/run-jobs.php">Run Jobs</a>
+    </div>
+    <div class="btn">
+      Download Files
+    </div>
+  </div>
+  <!-- Buttons ended -->
+
+  <div class="dashboard">
+    <h2 class="sspro black">Your Warehouse Dashboard</h2>
+    <p class="sspro inverted f16">Please set your preferences.</p>
+  </div>
+</body>
+</html>
